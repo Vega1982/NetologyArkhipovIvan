61ec4a8308c0aa149c5e2e98c183752efbfdcfd9
Ivan <vega1982ivan@gmail.com>
2022-06-03 13:17:49 +0700

    1st

--- src/main/java/firstDay/Main.java
+++ src/main/java/firstDay/Main.java
@@ -1,32 +1,52 @@
 package firstDay;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
+    // задаём слово
     static String word = "javalove";
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // javalove --------
+
         System.out.println("Угадай слово");
         int lengthWord = word.length();
+
+        // задаём прочерки на неизвестных буквах
         String maskWord = "-".repeat(lengthWord);
         System.out.println(maskWord);
 
-        // --------
-        // a
-        // -a-a----
-        // o
-        // -a-a-o--
+        // Переменная для счётчика ходов
+        int counterValue = 0;
+
+        // Массив введённых букв для проверки повторений
+        StringBuilder lastChars = new StringBuilder();
+
         do {
-            System.out.println("Введите буква");
+            System.out.println("Введите букву");
+            // Получаем от игрока букву
             char c = input.next().charAt(0);
-            // a 2
-            // w -1
+
+            // Проверяем, нет ли повторения ввода букв. Просим ввода уникальной буквы
+            while (repeaterChecker(lastChars, c)) {
+                System.out.println("Вы повторяетесь, уважаемый(ая)! Введите другую букву.");
+                c = input.next().charAt(0);
+            }
+            // добавляем введённую букву в массив ранее введённых
+            lastChars.append(c);
+
+            // Инкрементируем число ходов
+            counterValue++;
+
+
+            // Если буква есть в слове
             if (word.indexOf(c) >= 0) {
                 System.out.println("Удача");
+
+                // Перебираем слово, заменяет маску на букву от игрока при совпадении
                 for (char elem : word.toCharArray()) {
                     if (elem == c) {
                         maskWord = replaceMaskLetter(c, maskWord);
@@ -38,12 +58,19 @@ public class Main {
                 System.out.println(maskWord);
             }
         } while (maskWord.contains("-"));
-        System.out.println("Поздравляем ты выйграл");
+        System.out.println("Поздравляем! Вы выиграли!");
+        System.out.println("Букв в загаданном слове: " + word.length());
+        System.out.println("Вы затратили попыток: " + counterValue);
+        effCounter(counterValue, word.length());
+        System.out.println("Ваш приз: " + counterValue);
+
     }
 
-    /*
-    -a-a---- j
-    ja-a----
+
+    /**
+     * @param c        буква от игрока
+     * @param maskWord слово из прочерков (и) найденными буквами
+     * @return маска с найденными буквами
      */
     public static String replaceMaskLetter(char c, String maskWord) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -57,5 +84,24 @@ public class Main {
             }
         }
         return stringBuilder.toString();
+    }
+
+    // Метод для проверки повтора введённой буквы
+    // Сравнивает ввод пользователя со всеми предыдущими введёнными буквами
+    // Если есть совпадение - возвращает true, иначе false
+    public static boolean repeaterChecker(StringBuilder lastChars, char c) {
+        //String tempString = lastChars.toString();
+        if (lastChars.indexOf(String.valueOf(c)) == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    // метод для подсчёта эффективности
+    public static void effCounter(int counterValue, int worldLength) {
+        double eff = ((worldLength / counterValue) * 100.00D);
+        //DecimalFormat decimalFormat = new DecimalFormat( "#.###" );
+        //String result = decimalFormat.format(eff);
+        System.out.println("Ваша эффективность: " + eff + "%");
     }
 }
